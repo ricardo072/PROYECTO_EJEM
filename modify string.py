45cf7de8c57029ac62037f581affd8ad04d64b63
ricardo072 <ricardorodriguez26658023@gmail.com>
2024-12-19 09:07:49 -0600

    interacion desde casa , usando modificacion de string

---
+++ modify string.py
@@ -0,0 +1,15 @@
+ciudad='barinas'
+convertir_mayusculas=ciudad.upper()
+print('covertidos a mayusculas ',convertir_mayusculas)
+
+
+estado='BARINAS'
+convertir_minusculas=estado.lower()
+print('covertidos a minusculas   ',convertir_minusculas)
+
+comida_tipica='      es el pabellon     '
+remove_space=comida_tipica.strip()
+print('whit space ', comida_tipica, ' sin espacios', remove_space)
+
+remplazar=estado.replace('el estado es barinas','otra cosa')
+print('txt replace ',remplazar)
